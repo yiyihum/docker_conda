5a5ac93f07b80ceba7dbe7107c67ca9dd4a88e5d
yiming.111 <yiming.111@bytedance.com>
2025-04-09 09:44:12 +0000

    fix

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
-bash /mnt/bn/tiktok-mm-5/aiic/users/yiming/code/set_task/set_up_task.sh jtauber__termdoc-23 "jtauber/termdoc" 5de2d3d8ee9965da1a05a9b069683a7e1073f005 > /mnt/bn/tiktok-mm-5/aiic/users/yiming/test_log/2025-04-09-09/setup_log/jtauber__termdoc-23.log 2>&1
-bash /mnt/bn/tiktok-mm-5/aiic/users/yiming/code/set_task/env_init.sh jtauber__termdoc-23 5de2d3d8ee9965da1a05a9b069683a7e1073f005 > /mnt/bn/tiktok-mm-5/aiic/users/yiming/test_log/2025-04-09-09/init_log/jtauber__termdoc-23.log 2>&1
+mkdir /tmp/init_log
+mkdir /tmp/setup_log
+
+bash set_up_task.sh jtauber__termdoc-23 "jtauber/termdoc" 5de2d3d8ee9965da1a05a9b069683a7e1073f005 > /tmp/init_log/jtauber__termdoc-23.log 2>&1
+bash env_init.sh jtauber__termdoc-23 5de2d3d8ee9965da1a05a9b069683a7e1073f005 > /tmp/setup_log/jtauber__termdoc-23.log 2>&1
